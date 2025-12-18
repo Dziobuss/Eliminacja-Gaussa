99b6a9a7404d0fc4c2122cda1560cdaf32223d4b
Dziobuss <01206775@pw.edu.pl>
2025-12-18 19:50:36 +0100

    Eliminacja
    Zaimplementowalem funkcje eliminacji gaussa do projektu

--- src/gauss.c
+++ src/gauss.c
@@ -5,10 +5,32 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
-
-		return 0;
+	double temp;
+	for (int i =0;i<mat->r;i++){
+		temp = mat->data[i][i];
+		if (temp ==0){
+			return 1;
+		}
+		for(int j = i;j<mat->r;j++){ //podziel wiersz
+			mat->data[i][j] = mat->data[i][j]/temp;
+		}
+		for (int j = 0; j<mat->r;j++){
+			b->data[i][j] = b->data[i][j]/temp;
+		}
+		for (int j = 0;j<mat->r;j++){ //odejmij kolejne wiersze
+			if (i==j){
+				continue;
+			}
+			temp = mat->data[j][i];
+			for(int k = i;k<mat->r;k++){
+				mat->data[j][k] = mat->data[j][k] - temp*mat->data[i][k];
+			}
+		
+		
+			for(int k = 0;k<mat->r;k++){
+				b->data[j][k] = b->data[j][k] - temp*b->data[i][k];
+				}
+			}
+	}
+	return 0;
 }
-
