6f2386097871edca3e687bd004cbaef6cfe251dd
Dziobuss <01206775@pw.edu.pl>
2026-01-04 14:08:23 +0100

    Poprawa gauss.c
    Z powodu braku kompatybilności poprzedniej wersji funkcji eliminate() z backsubst() należało ją przeredagować tak aby zastosowanie funkcji backsubst miało również sens. Zmieniono przykladowe pliki danych.

--- src/gauss.c
+++ src/gauss.c
@@ -1,36 +1,28 @@
 #include "gauss.h"
 
 /**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
+ * Zwraca 0 - eliminacja zakonczona sukcesem
+ * Zwraca 1 - macierz osobliwa (pivot = 0)
  */
-int eliminate(Matrix *mat, Matrix *b){
-	double temp;
-	for (int i =0;i<mat->r;i++){
-		temp = mat->data[i][i];
-		if (temp ==0){
-			return 1;
-		}
-		for(int j = i;j<mat->r;j++){ //podziel wiersz
-			mat->data[i][j] = mat->data[i][j]/temp;
-		}
-		for (int j = 0; j<mat->r;j++){
-			b->data[i][j] = b->data[i][j]/temp;
-		}
-		for (int j = 0;j<mat->r;j++){ //odejmij kolejne wiersze
-			if (i==j){
-				continue;
-			}
-			temp = mat->data[j][i];
-			for(int k = i;k<mat->r;k++){
-				mat->data[j][k] = mat->data[j][k] - temp*mat->data[i][k];
-			}
-		
-		
-			for(int k = 0;k<mat->r;k++){
-				b->data[j][k] = b->data[j][k] - temp*b->data[i][k];
-				}
-			}
-	}
-	return 0;
+int eliminate(Matrix *mat, Matrix *b)
+{
+    double factor;
+    for (int i = 0; i < mat->r - 1; i++) {
+        if (mat->data[i][i] == 0) {
+            return 1;
+        }
+        for (int j = i + 1; j < mat->r; j++) {
+
+            factor = mat->data[j][i] / mat->data[i][i];
+            for (int k = i; k < mat->c; k++) {
+                mat->data[j][k] -= factor * mat->data[i][k];
+            }
+            b->data[j][0] -= factor * b->data[i][0];
+        }
+    }
+    if (mat->data[mat->r - 1][mat->r - 1] == 0) {
+        return 1;
+    }
+
+    return 0;
 }
